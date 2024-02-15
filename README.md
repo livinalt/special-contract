1cb7c234dd5eedac0e36cb643de57ea577cc5a03
Jeremiah Samuel <livinalt@gmail.com>
2024-02-15 14:48:30 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # special-contract
 An ERC20 contract with a special feature that deducts 10% of sender's balance when tokens are transferred
+
+
+    //write an ERC20 contract that has a special features 
+    //that anytime you are transferring tokens it deducts charges of 10% from sender's balance
+    // An erc20 contract follows some set of rules
+    // declaring token name, token abbrv, token decimals
+    // token total supply, 
---
+++ contracts/SpecialContract.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SpecialContract is ERC20 {
+
+    mapping(address=>uint256) balance;
+
+    event TranferedTo (uint256 indexed _value, address indexed _to);
+
+    constructor(string memory _tokenName, string memory _symbol, uint256 _initialSupply) ERC20(_tokenName, _symbol) {
+        _mint(msg.sender, _initialSupply);
+    }
+
+    
+    //function to enable transfer from the contract including a service fee of 10%
+    function transferToken(uint256 _value, address _to) public returns(uint256){
+        // uint256 coinSupply = totalSupply;
+        require(balance[msg.sender] > _value, "You dont have enough SCT tokens in your account");
+        require(_value > 0, "token amount must be greater than zero");
+        require(msg.sender != address(0), "Check this address again. You cannnot send to this address!"); // performing a sanity check
+        uint256 serviceFee = _value /10;
+        balance[msg.sender] = balance[msg.sender] - serviceFee; // added service fee here
+        balance[_to] = balance[_to] + _value; 
+        
+        emit TranferedTo(_value, _to);
+        return balance[msg.sender];
+    }
+
+    function tokenBalance(address _address) public view returns(uint256){
+        return balance[_address];
+    }
+
+}
